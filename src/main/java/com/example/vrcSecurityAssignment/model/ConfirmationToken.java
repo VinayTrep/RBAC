0eff0de09fe3e4bff3b9fd3401f916ecc3062345
VinayLJ <ljvinayofficial@gmail.com>
2024-12-19 13:22:49 +0530

    Some changes

--- src/main/java/com/example/vrcSecurityAssignment/model/ConfirmationToken.java
+++ src/main/java/com/example/vrcSecurityAssignment/model/ConfirmationToken.java
@@ -1,9 +1,11 @@
 package com.example.vrcSecurityAssignment.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Entity
 public class ConfirmationToken extends BaseModel{
 
